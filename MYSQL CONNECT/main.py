6931bc4f26f67c6d0075bc18b795f5cef6b6eb43
HiddenBlack <68143131+Hidden-black@users.noreply.github.com>
2023-10-25 20:34:41 +0530

    Update main.py

--- MYSQL CONNECT/main.py
+++ MYSQL CONNECT/main.py
@@ -2,20 +2,20 @@ import mysql.connector
 from mysql.connector import errorcode
 
 
-
 cnx = mysql.connector.connect(user='root',password='mysql',
                               host='127.0.0.1',database='student')
-
 
 e = cnx.cursor()
 
 #Run This when doing this for the first time
 # e.execute("Create database student")
-# e.execute("create table Student(name varchar(20),Rollno int,Class varchar(5),Sec varchar(3),Address varchar(20),City varchar(20),Pin int,Pc int)")
+# e.execute("create table Student(adm_no int PRIMARY KEY,name varchar(20),Rollno int,Class varchar(5),Sec varchar(3),Address varchar(35),City varchar(20),Pin int,Pc bigint)")
+
+
 v=int(input("ENTER THE SERIAL NUMBER\n 1.Add Student\n 2.Show List\n"))
 
 if v==1:
-  
+  k=int(input("Enter the Adm Number: "))
   b=input("Enter Student Name: ")
   c=int(input("Enter Student RollNo: "))
   d=input("Enter Student Class: ")
@@ -26,21 +26,22 @@ if v==1:
   j=int(input("Enter Student Contact: "))
 
   add_student = ("INSERT INTO Student "
-                  "(name,Rollno,Class,Sec,Address,City,Pin,Pc)"
-                  "VALUES (%s, %s, %s, %s, %s,%s, %s, %s)")
+                  "(adm_no,name,Rollno,Class,Sec,Address,City,Pin,Pc)"
+                  "VALUES (%s, %s, %s, %s, %s,%s, %s, %s, %s)")
 
-  studentdata= (f'{b}',c,f'{d}',f'{f}',f'{g}',f'{h}',i,j)
+  studentdata= (k,f'{b}',c,f'{d}',f'{f}',f'{g}',f'{h}',i,j)
   e.execute(add_student,studentdata)
   cnx.commit()
+  
 elif v==2:
-  j=int(input("Enter the student Rollno: "))
-  query = (f"SELECT * FROM Student WHERE Rollno = {j}")
+  j=int(input("Enter the Adm Number: "))
+  query = (f"SELECT * FROM Student WHERE adm_no = {j}")
   a="Army Public School Bolarum"
 
   e.execute(query)
-  for (name,Rollno,Class,Sec,Address,City,Pin,Pc) in e:
+  for (adm_no,name,Rollno,Class,Sec,Address,City,Pin,Pc) in e:
     # print(f"{name} {Rollno} {Class} {Sec} {Address} {City} {Pin} {Pc}")
-    print(f"\t  {a}\nName: {name}\t\t Class: {Class} {Sec}\nAddress: {Address}\nContact: {Pc}")
+    print(f"\t  {a}\nName: {name}\t\t Class: {Class} {Sec} /{Rollno}\nAddress: {Address}\nContact: {Pc}\tAdm No.: {adm_no}")
 
 
 
